84a2c48fc6fb4dfd099701f461575413e20ad783
Vasiliy <kkolyasev1228@gmail.com>
2020-06-24 00:26:31 +0300

    footer

--- footer.php
+++ footer.php
@@ -1,3 +1,26 @@
+<footer>
+    <div class="row">
+
+        <div class="twelve columns">
+
+			<?wp_nav_menu([
+				'theme_location'  => 'footer_menu',
+			]); ?>
+
+
+            <ul class="copyright">
+                <li>Copyright &copy; 2014 Sparrow</li>
+                <li>Design by <a href="http://www.styleshout.com/">Styleshout</a></li>
+            </ul>
+            <div id="go-top" style="display: block;"><a title="Back to Top" href="#">Go To Top</a></div>
+
+        </div>
+
+
+
+    </div>
+</footer>
+
 <? wp_footer(); ?>
 </body>
 </html>
