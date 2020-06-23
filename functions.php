a4458e0969512af203ad60eba604473bb90146b7
Vasiliy <kkolyasev1228@gmail.com>
2020-06-23 23:53:43 +0300

    menu

--- functions.php
+++ functions.php
@@ -1,13 +1,34 @@
 <?php
 
 add_action('wp_enqueue_scripts', 'my_wp_head' ); // хук автоматом сработает во время wp_head
-add_action('wp_enqueue_scripts', 'my_wp_footer' ); // хук автоматом сработает во время wp_head
+
+add_action('wp_enqueue_scripts', 'my_wp_footer' ); // хук автоматом сработает во время wp_footer
+
+add_action( 'after_setup_theme', function(){
+	register_nav_menus( [
+		'header_menu' => 'Меню в шапке',
+		'footer_menu' => 'Меню в подвале'
+	] );
+} );
+
+add_action('after_setup_theme', 'different');
+
+function different ()
+{
+	add_theme_support('title-tag'); //выводит title страницы автоматически
+}
+
+add_filter( 'document_title_separator', 'filter_function_name_4326' );
+function filter_function_name_4326( $sep ){
+	$sep = ' | ';
+
+	return $sep;
+}
 
 function my_wp_head ()
 {
 	wp_enqueue_style('style', get_stylesheet_uri());
-	wp_enqueue_style( 'bootstrap', get_template_directory_uri() .'assets/css/bootstrap.css');
-	wp_enqueue_style( 'bootstrap.min', get_template_directory_uri() .'assets/css/bootstrap.min.css');
+	wp_enqueue_style( 'bootstrap.min', get_template_directory_uri() .'/assets/css/bootstrap.min.css');
 }
 
 function my_wp_footer ()
@@ -15,10 +36,11 @@ function my_wp_footer ()
 	wp_deregister_script('jquery');
 	wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
 	wp_enqueue_script('jquery');
-	wp_enqueue_script('bootstrap.js', get_template_directory_uri(). 'assets/js/bootstrap.js', array(), null, true);
-	wp_enqueue_script('bootstrap.min.js', get_template_directory_uri(). 'assets/js/bootstrap.min.js', array(), null, true);
-	wp_enqueue_script('doubletaptogo.js', get_template_directory_uri(). 'assets/js/doubletaptogo.js', array(), null, true);
-	wp_enqueue_script('flexslider', get_template_directory_uri(). 'assets/js/jquery.flexslider.js', ['jquery'], null, true);
-	wp_enqueue_script('init', get_template_directory_uri(). 'assets/js/init.js', array(), null, true);
+	wp_enqueue_script('bootstrap.js', get_template_directory_uri(). '/assets/js/bootstrap.js', array(), null, true);
+	wp_enqueue_script('bootstrap.min.js', get_template_directory_uri(). '/assets/js/bootstrap.min.js', array(), null, true);
+	wp_enqueue_script('doubletaptogo.js', get_template_directory_uri(). '/assets/js/doubletaptogo.js', array(), null, true);
+	wp_enqueue_script('flexslider', get_template_directory_uri(). '/assets/js/jquery.flexslider.js', ['jquery'], null, true);
+	wp_enqueue_script('init', get_template_directory_uri(). '/assets/js/init.js', array(), null, true);
 
 }
+
--- header.php
+++ header.php
@@ -9,4 +9,26 @@
 
 	<title>Hello, world!</title>
 </head>
+
+<header>
+
+
+    <div class="twelve columns row">
+
+        <a href="<?=home_url() ?>">
+            <img style="width: 50px" src="https://logotab.ru/storage/logotypes/491/krylya.jpg">
+        </a>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <?wp_nav_menu([
+                    'theme_location'  => 'header_menu',
+                    'container_class' => 'collapse navbar-collapse',
+                    'container_id'         => '',
+                    'menu_class'      => 'navbar-nav',
+                    'menu_id'         => '',
+                ]); ?>
+        </nav>
+
+
+</header> <!-- Header End -->
 <body>
