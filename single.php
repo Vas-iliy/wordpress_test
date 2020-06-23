5d474db49f467b24c34d4fbd9f08eee840da1a59
Vasiliy <kkolyasev1228@gmail.com>
2020-06-23 22:56:34 +0300

    single

---
+++ single.php
@@ -0,0 +1,18 @@
+<? get_header(); ?>
+
+<? while (have_posts()): the_post(); ?>
+	<div class="col-md-12">
+		<div class="card">
+			<div class="card-header">
+				<h5 class="card-title"><? the_title(); ?></h5>
+			</div>
+
+			<div class="card-body">
+				<p class="card-text"><? the_excerpt(); ?></p>
+			</div>
+
+		</div>
+	</div>
+<?endwhile;?>
+
+<? get_footer(); ?>
