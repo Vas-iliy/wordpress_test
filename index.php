5b087dc4428d9337431f412897aa42e0b50305cc
Vasiliy <kkolyasev1228@gmail.com>
2020-06-23 22:54:21 +0300

    cart

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <? get_header(); ?>
+
 <h1>Hello, world!</h1>
+
+<? if (have_posts()): while (have_posts()): the_post(); ?>
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title"><? the_title(); ?></h5>
+            </div>
+
+            <div class="card-body">
+                <p class="card-text"><? the_excerpt(); ?></p>
+            </div>
+
+            <div class="card-footer">
+                <a href="<? the_permalink(); ?>" class="btn btn-primary">Читать далее</a>
+            </div>
+        </div>
+    </div>
+<?endwhile;?>
+<?endif;?>
+
 <? get_footer(); ?>
