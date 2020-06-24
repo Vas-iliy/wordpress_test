05f94f66ef7c807e6d2e65c806a1639ba6a51f11
Vasiliy <kkolyasev1228@gmail.com>
2020-06-24 05:18:45 +0300

    page

---
+++ page.php
@@ -0,0 +1,22 @@
+<? get_header(); ?>
+
+<? while (have_posts()): the_post(); ?>
+	<div class="col-md-12">
+		<div class="card">
+			<div class="card-header">
+				<h5 class="card-title"><? the_title(); ?></h5>
+			</div>
+
+			<div class="card-body">
+				<?if (has_post_thumbnail()):
+					the_post_thumbnail(); else:?>
+					<img src="https://picsum.photos/1275/638" alt="">
+				<?endif;?>
+				<p class="card-text"><? the_content(); ?></p>
+			</div>
+
+		</div>
+	</div>
+<?endwhile;?>
+
+<? get_footer(); ?>
