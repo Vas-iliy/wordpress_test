0661dfb336475e88e04b76259999fef9db86c07f
Vasiliy <kkolyasev1228@gmail.com>
2020-06-24 06:52:26 +0300

    paginations

--- index.php
+++ index.php
@@ -3,30 +3,39 @@
 <div class="container">
     <div class="row">
         <div class="col">
-	        <? if (have_posts()): while (have_posts()): the_post(); ?>
-                <div class="col">
-                    <div class="card row">
-                        <div class="card-header">
-                            <h5 class="card-title"><? the_title(); ?></h5>
-                        </div>
+            <?/*query_posts('posts_per_page=3')*/?>
 
-                        <div class="card-body">
-					        <?if (has_post_thumbnail()):
-						        the_post_thumbnail('anime', ['class' => 'float-left mr-3']);
-					        else:?>
-                                <img src="https://picsum.photos/150/150" width="150px" height="150px" alt="" class="float-left mr-3">
-					        <?endif;?>
-                            <p class="card-text"><? the_excerpt(); ?></p>
-                        </div>
+	    <? if (have_posts()): while (have_posts()): the_post(); ?>
+            <div class="col">
+                <div class="card row">
+                    <div class="card-header">
+                        <h5 class="card-title"><? the_title(); ?></h5>
+                    </div>
 
-                        <div class="card-footer">
-                            <a href="<? the_permalink(); ?>" class="btn btn-primary">Читать далее</a>
-                        </div>
+                    <div class="card-body">
+					    <?if (has_post_thumbnail()):
+						    the_post_thumbnail('anime', ['class' => 'float-left mr-3']);
+					    else:?>
+                            <img src="https://picsum.photos/150/150" width="150px" height="150px" alt="" class="float-left mr-3">
+					    <?endif;?>
+                        <p class="card-text"><? the_excerpt(); ?></p>
+                    </div>
+
+                    <div class="card-footer">
+                        <a href="<? the_permalink(); ?>" class="btn btn-primary">Читать далее</a>
                     </div>
                 </div>
-	        <?endwhile;?>
-	        <?endif;?>
-        </div>
+            </div>
+	    <?endwhile;?>
+        <? the_posts_pagination([
+			    'end_size'     => 1,
+			    'mid_size'     => 1,
+			    'prev_text'    => __('« Previous'),
+			    'next_text'    => __('Next »'),
+		    ]); ?>
+	    <?endif;?>
+    </div>
+
 
 	    <? get_sidebar(); ?>
     </div>
